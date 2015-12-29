a04921609eeca39dafbd7b1c8f0a3c398c3fdc8a
Victor Tang <victortang1992@gmail.com>
2015-12-29 00:23:13 -0800

    stores fin

--- flux-via-alt-site/stores/LocationStores.js
+++ flux-via-alt-site/stores/LocationStores.js
@@ -1,9 +1,18 @@
 var alt = require('../alt');
+var LocationActions = require('../actions/LocationActions');
 
 class LocationStore {
   constructor() {
-	  this.locations = [];
+    this.locations = [];												//all stores have space to store things
+
+    this.bindListeners({												//binding action handlers
+      handleUpdateLocations: LocationActions.UPDATE_LOCATIONS
+    });
+  }
+
+  handleUpdateLocations(locations) {									//each store handles own locations storage upon action
+    this.locations = locations;
   }
 }
 
-module.exports = alt.createActions(LocationActions);
+module.exports = alt.createStore(LocationStore, 'LocationStore');
