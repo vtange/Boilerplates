d2f5c907e1ab895f9fb5e9ec4d7263474517314b
Victor Tang <victortang1992@gmail.com>
2015-12-29 16:53:59 -0800

    init static_angular_alt

---
+++ static_angular_alt_site/app.js
@@ -0,0 +1,10 @@
+(function() {
+    //start of function
+  var app = angular.module('ThisApp', []);
+
+app.controller('CatalogCtrl', ['$scope', function($scope){
+
+
+}]);//end of controller
+  //end of function
+})();
