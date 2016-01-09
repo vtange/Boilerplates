fd78b5b73fe9e7f4798783ab455ff1414828a4fa
Victor Tang <victortang1992@gmail.com>
2016-01-09 01:06:04 -0800

    fadey animation

--- css/glowgridAnimation/anim.js
+++ css/glowgridAnimation/anim.js
@@ -25,22 +25,21 @@
 	function glow(){
 		var x = Math.floor(Math.random()*numCols);
 		var y = Math.floor(Math.random()*numRows);
-
 		var r = Math.floor(Math.random()*(255-150)+150);
 		var g = Math.floor(Math.random()*(255-150)+150);
 		var b = Math.floor(Math.random()*(255-150)+150);
 		var size = Math.floor(Math.random()*5)*50+50;
-		var brush = $('<div id="glow'+y+'-'+x+'" class="glow-element" style="height:'+size+'px;width:'+size+'px;background-color:#7bb"></div>');
-		console.log(brush);
+		var brush = $('<div id="glow'+y+'-'+x+'" class="glow-element" style="height:'+size+'px;width:'+size+'px;background-color:rgba('+r+','+g+','+b+',0.6)"></div>');
 		$('#cell'+y+'-'+x).append(brush);
+		$('#glow'+y+'-'+x).velocity({ opacity: 1.0 }, [ 0.17, 0.67, 0.83, 0.67 ]);
 		//remove class after set amt of time
 		function unglow(){
-			$('#glow'+y+'-'+x).remove();
+			$('#glow'+y+'-'+x).velocity({ opacity: 0 }, [ 0.17, 0.67, 0.83, 0.67 ]);
+			setTimeout(function(){$('#glow'+y+'-'+x).remove()},600);
 		}
-		setTimeout(unglow,2000);
+		setTimeout(unglow,3000);
 	}
 	setInterval(glow,1800);
-	
-	
+	setInterval(glow,2400);
   //end of function
 })();
