025f4f81cfada98b8be414b1b14d42f24b5a1ee1
Victor Tang <victortang1992@gmail.com>
2016-01-08 20:37:46 -0800

    add glow

--- css/glowgridAnimation/anim.js
+++ css/glowgridAnimation/anim.js
@@ -1,12 +1,14 @@
 (function() {
     //start of function
+	var numCols;
+	var numRows;
 	function generateGrid(){
 		$('#grid').html("");
 		var whole = $('<div id="whole"></div>')
 		var row = $('<div class="cell-row"></div>')
 		var cellSize = 50;
-		var numCols = window.innerWidth % cellSize === 0? window.innerWidth / cellSize : Math.ceil(window.innerWidth / cellSize) ;
-		var numRows = window.innerHeight % cellSize === 0? window.innerHeight / cellSize : Math.ceil(window.innerHeight / cellSize) ;
+		numCols = window.innerWidth % cellSize === 0? window.innerWidth / cellSize : Math.ceil(window.innerWidth / cellSize) ;
+		numRows = window.innerHeight % cellSize === 0? window.innerHeight / cellSize : Math.ceil(window.innerHeight / cellSize) ;
 		for (var i = 0; i < numRows; i++){
 			row = $('<div class="cell-row"></div>')
 			for (var j = 0; j < numCols; j++){
@@ -19,6 +21,22 @@
 	};
 	generateGrid();
 	$(window).resize(generateGrid);
+	function glow(){
+		var x = Math.floor(Math.random()*numCols);
+		var y = Math.floor(Math.random()*numRows);
+		console.log(x+","+y)
+		var r = Math.floor(Math.random()*(255-150)+150);
+		var g = Math.floor(Math.random()*(255-150)+150);
+		var b = Math.floor(Math.random()*(255-150)+150);
+		document.getElementById('row'+y+'num'+x).style.backgroundColor='rgb('+r+','+g+','+b+')';
+		function unglow(){
+			document.getElementById('row'+y+'num'+x).style.backgroundColor='';
+		}
+		//remove class after set amt of time
+		setTimeout(unglow,1000);
+	}
+	setInterval(glow,750);
+	
 	
   //end of function
 })();
