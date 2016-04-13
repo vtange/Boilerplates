0307fef7d04e7a07f178222f41b24753f006b8c3
Victor Tang <victortang1992@gmail.com>
2016-04-13 14:00:33 -0700

    db and header updates

--- site-w-logins-header/config/database.js
+++ site-w-logins-header/config/database.js
@@ -2,6 +2,6 @@
 // config/database.js
 module.exports = {
 
-    'url' :  process.env.MONGOLAB_URI || 'mongodb://localhost/stock' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' :  process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost/newapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
