9d15d464348efa0235232479a29d87aa487910dd
Victor Tang <victortang1992@gmail.com>
2016-04-20 00:52:39 -0700

    slushify old boilerplates

--- site-w-logins-header-spa/public/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    //start of function
-  var app = angular.module('ANGULAR_APP_NAME', ['header']);
-
-	
-  //end of function
-})();
--- site-w-logins-header/config/database.js
+++
@@ -1,7 +0,0 @@
-
-// config/database.js
-module.exports = {
-
-    'url' :  process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost/newapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
